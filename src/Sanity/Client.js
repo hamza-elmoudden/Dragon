3f053be293c1a98e64ac445f6d6778ac917ef5f9
hamza-elmoudden <hamzaelmouddane2000@gmail.com>
2024-05-14 08:40:08 +0100

    change

--- src/Sanity/Client.js
+++ src/Sanity/Client.js
@@ -2,10 +2,14 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from '@sanity/image-url'
 
 
+
+
 export const client = createClient({
         projectId: "m1wlz9v9",
         dataset: "production",
         useCdn: true,
+        token:"skgJubak8aZH3XIyNdT4fOGlXomCSlFg7HBoPCFy6CAaSqHFax1CsJGQjNEWVcYPSmmiSqVfY5ywMlMKIFF0nrkL8M2s6zRSldKZr71TrfAFXpRRkwLWMXZL7l3ZUZ9klXtvGCKbi8Fl2I4MaTNbJwdArApX61A5WgErMNGMsYHuVVugOGYW"
+
 })
 
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {store} from './Redux/Stor'
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
